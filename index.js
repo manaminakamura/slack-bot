d1fe9464a8a8e2cdd6b88ed7a6f4f754d2c70938
Manami Nakamura <manaminakamura812@gmail.com>
2018-07-22 21:01:46 +0900

    lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Twitter = require('twitter');
+const Twitter = require("twitter");
 
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,28 +11,28 @@ const client = new Twitter({
 
 const Botkit = require("botkit");
 const controller = Botkit.slackbot({
-  debug: false
+    debug: false
 });
 
 const bot = controller.spawn({
-  token: process.env.SLACK_BOTS_TOKEN
+    token: process.env.SLACK_BOTS_TOKEN
 }).startRTM();
 
 controller.hears(
-  ["(.*)"], "direct_message,direct_mention,mention", (bot, message) => {
-    const userId = process.env.TWITTER_TGT_USER_ID;
-    const params = { q: `${message.text} from:${userId}`, count: 1 };
-    client.get('search/tweets', params, (error, tweets) => {
-      if(error) {
-        console.log(error);
-        bot.reply(message, "An error has occured.");
-        return;
-      }
-      if(tweets.statuses.length == 0) {
-        bot.reply(message, "No data");
-        return;
-      }
-      bot.reply(message, tweets.statuses[0].text);
-    });
-  }
+    ["(.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+        const userId = process.env.TWITTER_TGT_USER_ID;
+        const params = { q: `${message.text} from:${userId}`, count: 1 };
+        client.get("search/tweets", params, (error, tweets) => {
+            if(error) {
+                console.log(error);
+                bot.reply(message, "An error has occured.");
+                return;
+            }
+            if(tweets.statuses.length == 0) {
+                bot.reply(message, "No data");
+                return;
+            }
+            bot.reply(message, tweets.statuses[0].text);
+        });
+    }
 );
