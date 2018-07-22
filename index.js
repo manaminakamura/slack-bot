9cb7fe6bcff35efda263974a2a00d658a3e5ca09
Manami Nakamura <manaminakamura812@gmail.com>
2018-07-22 20:19:38 +0900

    入力語でツイート検索

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
-if(!process.env.SLACK_BOTS_TOKEN) {
-  console.log("Error: Specify token in environment");
-  process.exit(1);
-}
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 const Botkit = require("botkit");
 const controller = Botkit.slackbot({
@@ -15,7 +19,19 @@ const bot = controller.spawn({
 }).startRTM();
 
 controller.hears(
-  ["hi"],
-  "direct_message,direct_mention,mention",
-  (bot, message) => { bot.reply(message, 'bye') }
+  ["(.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+    const params = { q: `${message.text} filter:images`, count: 1 };
+    client.get('search/tweets', params, (error, tweets) => {
+      if(error) {
+        console.log(error);
+        bot.reply(message, "An error has occured.");
+        return;
+      }
+      if(tweets.statuses.length == 0) {
+        bot.reply(message, "no data");
+        return;
+      }
+      bot.reply(message, tweets.statuses[0].text);
+    });
+  }
 );
