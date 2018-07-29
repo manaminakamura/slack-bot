93dc367372b3429070c91942cc634db76eb65f26
Manami Nakamura <manaminakamura812@gmail.com>
2018-07-29 23:12:39 +0900

    とりあえずany

--- index.ts
+++ index.ts
@@ -1,28 +1,28 @@
 require("dotenv").config();
 
-const Twitter = require("twitter");
+const Twitter: any = require("twitter");
 
-const client = new Twitter({
+const client: any = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const Botkit = require("botkit");
-const controller = Botkit.slackbot({
+const Botkit: any = require("botkit");
+const controller: any = Botkit.slackbot({
     debug: false
 });
 
-const bot = controller.spawn({
+const bot: any = controller.spawn({
     token: process.env.SLACK_BOTS_TOKEN
 }).startRTM();
 
 controller.hears(
-    ["(.*)"], "direct_message,direct_mention,mention", (bot, message) => {
-        const userId = process.env.TWITTER_TGT_USER_ID;
-        const params = { q: `${message.text} from:${userId}`, count: 1 };
-        client.get("search/tweets", params, (error, tweets) => {
+    ["(.*)"], "direct_message,direct_mention,mention", (bot: any, message: any) : any => {
+        const userId: any = process.env.TWITTER_TGT_USER_ID;
+        const params: any = { q: `${message.text} from:${userId}`, count: 1 };
+        client.get("search/tweets", params, (error: any, tweets: any) : any => {
             if(error) {
                 console.log(error);
                 bot.reply(message, "An error has occured.");
